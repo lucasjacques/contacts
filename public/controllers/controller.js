5555aeed8ecf357fa9bab0dc3a6a9c1b67f51ff9
Lucas Jacques <lucas.jacs@gmail.com>
2019-02-08 01:42:41 -0200

    Now using "catch" to catch errors

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,14 +1,16 @@
 let myApp = angular.module('myApp', []); 
 
 (function(app){
-  "use strict";
-  app.controller('AppCtrl', function($scope, $http){
-	$http.get('/contactList').then(function (response) {
-		console.log('I got the data I requested');
-		$scope.contactList = response.data;
-	},
-	function (error) {
-		console.log('An error has been found', error);
+	"use strict";
+	app.controller('AppCtrl',
+	function($scope, $http){
+		$http.get('/contactList').then(
+		function (response) {
+			console.log('I got the data I requested');
+			$scope.contactList = response.data;
+		}).catch(
+		function (error) {
+			console.log('An error has been found', error);
+		});
 	});
-  });
 })(myApp);
