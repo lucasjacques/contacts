208e8851cb7fd77fe263b3ab561e415718caa302
Lucas Jacques <lucas.jacs@gmail.com>
2019-02-08 01:32:17 -0200

    Added GET request "contactList" and its hooks

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -3,6 +3,12 @@ let myApp = angular.module('myApp', []);
 (function(app){
   "use strict";
   app.controller('AppCtrl', function($scope, $http){
-    console.log('Hello People');
+	$http.get('/contactList').then(function (response) {
+		console.log('I got the data I requested');
+		$scope.contactList = response.data;
+	},
+	function (error) {
+		console.log('An error has been found', error);
+	});
   });
 })(myApp);
--- server.js
+++ server.js
@@ -3,6 +3,28 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/contactList', function (req, res) {
+	console.log('I received a GET request');
+	let person1 = {
+    	name : 'Jacs',
+    	email : 'email@test.com',
+    	number : '1337' 
+    },
+  	person2 = {
+    	name : 'Alice',
+    	email : 'email@test.com',
+    	number : '666' 
+    },
+    person3 = {
+    	name : 'Bob',
+    	email : 'email@test.com',
+    	number : '11233123' 
+    };
+
+    let contactList = [person1, person2, person3];
+    res.json(contactList);
+})
+
 let port = 3500;
 app.listen(port);
 console.log('Server running on port ' + port);
