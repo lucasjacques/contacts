de3261336ad0037624a6376a8686a51260af1f3d
Lucas Jacques <lucas.jacs@gmail.com>
2019-02-11 13:23:55 -0200

    Added mongodb integration and addContact function

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -4,13 +4,25 @@ let myApp = angular.module('myApp', []);
 	"use strict";
 	app.controller('AppCtrl',
 	function($scope, $http){
-		$http.get('/contactList').then(
-		function (response) {
-			console.log('I got the data I requested');
-			$scope.contactList = response.data;
-		}).catch(
-		function (error) {
-			console.log('An error has been found', error);
-		});
+		let refresh = function () {
+			$http.get('/contactList').then(
+			function (response) {
+				console.log('I got the data I requested');
+				$scope.contactList = response.data;
+				$scope.contact = {};
+			}).catch(
+			function (error) {
+				console.log('An error has been found', error);
+			});
+		};
+		refresh();
+		$scope.addContact = function () {
+			console.log($scope.contact);
+			$http.post('/contactList', $scope.contact).then(
+			function (response) {
+				console.log(response);
+				refresh();
+			});
+		}
 	});
 })(myApp);
--- server.js
+++ server.js
@@ -1,29 +1,25 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('contactlist', ['contactlist']);
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
 app.get('/contactList', function (req, res) {
 	console.log('I received a GET request');
-	let person1 = {
-    	name : 'Jacs',
-    	email : 'email@test.com',
-    	number : '1337' 
-    },
-  	person2 = {
-    	name : 'Alice',
-    	email : 'email@test.com',
-    	number : '666' 
-    },
-    person3 = {
-    	name : 'Bob',
-    	email : 'email@test.com',
-    	number : '11233123' 
-    };
-
-    let contactList = [person1, person2, person3];
-    res.json(contactList);
-})
+	db.contactlist.find(function (err, docs) {
+        console.log(docs);
+        res.json(docs);
+    });
+});
+app.post('/contactList',function (req, res) {
+    console.log(req.body);
+    db.contactlist.insert(req.body, function (err, doc) {
+        res.json(doc);
+    })
+});
 
 let port = 3500;
 app.listen(port);
