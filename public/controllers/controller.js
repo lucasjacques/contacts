d446f383224977927919485c62aee8251ead0f54
Lucas Jacques <lucas.jacs@gmail.com>
2019-02-16 03:07:56 -0200

    Update contacts feature done

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -16,10 +16,12 @@ let myApp = angular.module('myApp', []);
 		};
 		refresh();
 		$scope.addContact = function () {
-			console.log($scope.contact);
 			$http.post('/contactList', $scope.contact).then(
 			function (response) {
 				refresh();
+			}).catch(
+			function (error) {
+				console.log('An error has been found', error);
 			});
 		}
 
@@ -27,15 +29,32 @@ let myApp = angular.module('myApp', []);
 			$http.delete('/contactList/' + id).then(
 				function (response) {
 					refresh();
+				}).catch(
+				function (error) {
+					console.log('An error has been found', error);
 				});
 		}
 		$scope.edit = function (id) {
-			console.log(id);
 			$http.get('/contactList/' + id).then(
 				function (response) {
-					$scope.contact = response;
-					// refresh();
+					$scope.contact = response.data;
+				}).catch(
+				function (error) {
+					console.log('An error has been found', error);
 				});
+		}
+		$scope.update = function () {
+			$http.put('/contactList/' + $scope.contact._id, $scope.contact).then( 
+				function (response) {
+					refresh();
+				}
+			).catch(
+			function (error) {
+				console.log('An error has been found', error);
+			});
+		}
+		$scope.deselect = function () {
+			$scope.contact = {};
 		}
 	});
 })(myApp);
